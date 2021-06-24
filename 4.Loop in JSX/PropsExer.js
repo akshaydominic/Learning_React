b09e3f1c7d256cff99eb47f3c477e94c84b4ce9d
Akshay <adominic21@gmail.com>
2021-06-25 01:05:05 +0530

    Styling added

--- 4.Loop in JSX/PropsExer.js
+++ 4.Loop in JSX/PropsExer.js
@@ -2,8 +2,8 @@ class PropsExer extends React.Component{
     render(){
         const {name,programming} = this.props;
         return(
-            <div>
-                <h2>{name}</h2>
+            <div style={{fontSize:"30px"}}>
+                <h2 className ="name">{name}</h2>
                 <ul>
                     {programming.map(m=><li><h3>{m}</h3></li>)}
                 
