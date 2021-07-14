cdac20936653bccdc551582d42f8d6cd0ec86fb7
Akshay <adominic21@gmail.com>
2021-07-14 11:31:22 +0530

    Ball component added

---
+++ 10.state_lottery/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Ball from './Ball'
+
+function App() {
+  return (
+    <div className="App">
+      <Ball num={10}/>
+      <Ball num={12}/>
+      <Ball num={13}/>
+      <Ball num={14}/>
+
+    </div>
+  );
+}
+
+export default App;
