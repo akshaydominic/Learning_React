725e8b0d27b48753987bf9213cf83a1e096ff836
Akshay <adominic21@gmail.com>
2022-03-07 03:39:27 +0530

    Refreshing Concepts

---
+++ 01.Refresh Basic/expensetracker/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <ExpenseItem />
+  );
+}
+
+export default App;
